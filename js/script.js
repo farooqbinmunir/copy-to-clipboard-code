3f277f6127ba47b95c87e414907a9d22f1c71b7d
Farooq Bin Munir <143595715+farooqbinmunir@users.noreply.github.com>
2023-09-26 16:24:00 +0500

    Updated

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 document.querySelector('#fbm-copy-btn').addEventListener('click', e => {
     var text = document.querySelector('p#fbm-text').innerText;
     window.navigator.clipboard.writeText(text).then(x => {
-        document.querySelector('.fbm-content-wrapper').append(`<span id="fbm-copied">Copied to clipboard</span>`);
+        document.querySelector('.fbm-content-wrapper').append('<span id="fbm-copied">Copied to clipboard</span>');
         setTimeout(() => {
             document.querySelector('#fbm-copied').remove();
         }, 5000);
